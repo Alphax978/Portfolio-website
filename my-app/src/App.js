4d40da895a4845809a20fcddebc11b3cc97e0cd3
Rawalss <valorenid@gmail.com>
2023-09-11 15:40:29 +0545

    About me

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,13 @@
+import About from "./Screens/About Me/About";
 import Intro from "./Screens/Introduction/Intro";
 import Navbar from "./Screens/Navbar/Navbar";
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
+      <About></About>
       
     </div>
   );
