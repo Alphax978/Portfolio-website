df37d75c3afac47ed915f1a148ebe6107610eb25
Rawalss <valorenid@gmail.com>
2024-02-05 19:31:25 +0545

    chatbot

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,9 +4,91 @@ import Navbar from "./Screens/Navbar/Navbar";
 import Work from "./Screens/Portfolio/Portfolio";
 import Contact from "./Screens/Contacts/Contact";
 import Footer from "./Screens/Footer/Footer";
+import Chatbot from 'react-simple-chatbot'
+import { ThemeProvider } from 'styled-components';
+import botavatarjr from "./assets/logos.png"
+import "./App.css"
 import {  useState } from "react";
 
 
+const steps = [
+  {
+      id: '1',
+      message: 'Hello, How can i help you on this fine day ?',
+      trigger:'2'
+  },
+  {
+    id: '2',
+    options: [
+      { value: 1, label: 'Contact me', trigger:'3'},
+      { value: 2, label: 'I want to See your Projects',trigger:'4' },
+      { value: 3, label: 'What Frameworks do you know?',trigger:'5' },
+      { value: 4, label: 'What is level of understanding on the frameworks that you know ?',trigger:'6'},
+      { value: 5, label: 'Which Unversity did you graduate from ?',trigger:'7'},
+      { value: 6, label: 'Are you available to hire right now ?',trigger:'8'},
+      { value: 7, label: 'close chat',trigger:'9'},
+
+    ],
+  },
+  {
+    id:'3',
+    message: 'Head over to contact me page. fill out the form and hit submit',
+    trigger:'2'
+  },
+  {
+    id: '4',
+    message: 'Head over to portfolio page. Check out the projects. More projects are yet to be added',
+    trigger:'2'
+    
+  },
+  {
+    id: '5',
+    message: 'I know frameworks like ReactJs, Bootstrap, Tailwindcss. I am also aware of version control like Git. I know Node.js and Figma too.',
+    trigger:'2'
+    
+  },
+  {
+    id: '6',
+    message: "I am aware of how it works and what to do. I have made some projects with those frameworks.",
+    trigger:'2'
+    
+  },
+  {
+    id: '7',
+    message: 'I Graduated from University of wolverhampton. I studied Bsc(Honors) Computer Science',
+    trigger:'2'
+    
+  },
+  {
+    id: '8',
+    message: 'I am available.',
+    trigger:'2'
+    
+  },
+  {
+    id:'9',
+    message:'see you again soon',
+    end:true
+  }
+];
+
+const theme = {
+  background: '#f5f8fb',
+  fontFamily: 'Helvetica Neue',
+  headerBgColor: '#F9DF0D',
+  headerFontColor: '#fff',
+  headerFontSize: '15px',
+  botBubbleColor: '#F9DF0D',
+  botFontColor: '#fff',
+  userBubbleColor: '#fff',
+  userFontColor: '#F9DF0D',
+}
+
+const config = {
+  // botAvatar: botavatarjr,
+  floating: true,
+
+};
 
 
 function App() {
@@ -16,11 +98,17 @@ function App() {
     setIsDark(!isDark)
   }
 
+ 
+
   return (
     <div style={{
       backgroundColor: isDark ? 'rgb(30,30,30)' : 'rgb(224,224,224)',
     }}>
-        <Navbar darkthemebutton={changetheme} />
+
+      <Navbar darkthemebutton={changetheme} />
+        <ThemeProvider theme={theme}>
+          <Chatbot steps={steps} {...config} headerTitle="James Rawal" speechSynthesis={{ enable: true, lang: 'en' }} />
+        </ThemeProvider>
         <Intro />
         <About theme={isDark} />
         <Work />
--- my-app/src/Screens/Introduction/Intro.js
+++ my-app/src/Screens/Introduction/Intro.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Intro.css"
 import { Link } from 'react-scroll'
+import { ThemeProvider } from 'styled-components';
 import bg from "../../assets/image.png"
 import btnImg from "../../assets/hireme.png"
+
+
+
 const Intro = () => {
+
     return (
         <section id="intro">
             <div className="introContent">
@@ -14,7 +19,9 @@ const Intro = () => {
                     I'm <span className="introName">James Rawal</span><br/>Website Developer
                 </span>
                 <p className="introPara">I am a skilled website developer <br />with adequate experience</p>
-                <Link to='contact'spy={true} smooth={true} offset={-100} duration={500}><button className='btn'><img className="btnImg" src={btnImg} alt="" />Hire Me</button></Link>
+                <Link to='contact' spy={true} smooth={true} offset={-100} duration={500}>
+                    <button className='btn'><img className="btnImg" src={btnImg} alt="" />Hire Me</button>
+                </Link>
             </div>
             <img src="" alt="profile" className="bg" />
 
